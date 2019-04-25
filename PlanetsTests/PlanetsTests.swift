fa73cff7668e2e56fb073df7fc52f6c8243e9f12
Gil Biren <gilbiren@gmail.com>
2019-04-25 09:40:42 +0200

    Add unit test example

---
+++ PlanetsTests/PlanetsTests.swift
@@ -0,0 +1,19 @@
+//
+//  PlanetsTests.swift
+//  PlanetsTests
+//
+//  Created by Gil Biren on 25/04/2019.
+//  Copyright © 2019 Rayan Slim. All rights reserved.
+//
+
+import XCTest
+@testable import Planets
+
+class PlanetsTests: XCTestCase {
+
+    func testExample() {
+        let sun = Planet.sun
+        XCTAssert(sun.distanceFromParent == 0, "NOPE")
+    }
+
+}
